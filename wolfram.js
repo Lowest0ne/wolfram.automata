bb762633c002971e14d249a5e16faf7518891bca
Carl Schwope <schwope.carl@gmail.com>
2013-09-19 14:36:01 -0400

    fill whole rectangle

--- wolfram.js
+++ wolfram.js
@@ -1,11 +1,11 @@
 function colorRow( imageData, row, width )
 {
-  for ( var i = 0; i < width; i += 4 )
+  for ( var i = 0; i < width * 4; i += 4 )
   {
-    imageData.data[ ( row * width ) + i + 0 ] = 0;
-    imageData.data[ ( row * width ) + i + 1 ] = 0;
-    imageData.data[ ( row * width ) + i + 2 ] = 0;
-    imageData.data[ ( row * width ) + i + 3 ] = 255;
+    imageData.data[ ( row * width * 4) + i + 0 ] = 0;
+    imageData.data[ ( row * width * 4) + i + 1 ] = 0;
+    imageData.data[ ( row * width * 4) + i + 2 ] = 0;
+    imageData.data[ ( row * width * 4) + i + 3 ] = 255;
   }
 
 }
@@ -13,15 +13,15 @@ function colorRow( imageData, row, width )
 function makeWolfram()
 {
   var canvas    = document.getElementById("wolfram");
+  var width     = canvas.getAttribute('width');
+  var height    = canvas.getAttribute('height');
   var context   = canvas.getContext("2d");
-  var imageData = context.createImageData(100,100);
-  var width     = imageData.width;
-  var height    = imageData.height;
+  var imageData = context.createImageData(width, height);
 
   for ( var row = 0; row < height; row += 1 )
   {
     colorRow( imageData, row, width );
   }
 
-  context.putImageData(imageData,10,10);
+  context.putImageData(imageData,0,0);
 }
