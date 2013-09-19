924c80a9bec7ae657f267245cba2aacfce6598ff
Carl Schwope <schwope.carl@gmail.com>
2013-09-19 16:45:01 -0400

    use jquery

--- wolfram.js
+++ wolfram.js
@@ -1,43 +1,55 @@
-function pixel( row, column )
-{
-  return row * column * 4;
-}
-
-function colorPixel( imageData, pixel )
-{
-  imageData.data[ pixel ] = 0;
-  imageData.data[ pixel + 1] = 0;
-  imageData.data[ pixel + 2] = 0;
-  imageData.data[ pixel + 3] = 255;
-}
-
-
-function colorRow( imageData, row, width )
-{
-  actual_width = width * 4
-  for ( var i = 0; i < actual_width; i += 4 )
+$(function() {
+  function pixel( row, column )
   {
-    if ( imageData.data[ pixel( row - 1, width )  + i + 3] == 0 )
+    return row * column * 4;
+  }
+
+  function colorPixel( imageData, pixel )
+  {
+    imageData.data[ pixel ]    = 0;
+    imageData.data[ pixel + 1] = 0;
+    imageData.data[ pixel + 2] = 0;
+    imageData.data[ pixel + 3] = 255;
+  }
+
+  function shouldReplicate( imageData, pixel )
+  {
+    return imageData.data[ pixel + 3 ] == 255;
+  }
+
+
+  function colorRow( imageData, row, width )
+  {
+    actual_width = width * 4;
+    for ( var i = 0; i < actual_width; i += 4 )
     {
-      colorPixel( imageData, pixel( row, width ) + i );
+      if ( shouldReplicate( imageData, pixel( row - 1, width )  + i) )
+      {
+        colorPixel( imageData, pixel( row, width ) + i );
+      }
     }
+
   }
 
-}
-
-function makeWolfram()
-{
-  var canvas    = document.getElementById("wolfram");
-  var width     = canvas.getAttribute('width');
-  var height    = canvas.getAttribute('height');
-  var context   = canvas.getContext("2d");
-  var imageData = context.createImageData(width, height);
-
-  imageData.data[width * 2 + 3] = 255;
-  for ( var row = 1; row < height; row += 1 )
+  function makeWolfram()
   {
-    colorRow( imageData, row, width );
+    var canvas    = document.getElementById("wolfram");
+    var width     = canvas.getAttribute('width');
+    var height    = canvas.getAttribute('height');
+    var context   = canvas.getContext("2d");
+    var imageData = context.createImageData(width, height);
+
+    imageData.data[width * 2 + 3] = 255;
+    for ( var row = 1; row < height; row += 1 )
+    {
+      colorRow( imageData, row, width );
+    }
+
+    context.putImageData(imageData,0,0);
   }
 
-  context.putImageData(imageData,0,0);
-}
+  $(".wolfram_button").click(makeWolfram );
+
+
+
+});
