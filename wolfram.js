a1a6ad26f99f886459ca60c3a5e67b84d1288dd9
Carl Schwope <schwope.carl@gmail.com>
2013-09-19 12:49:29 -0400

    draw red rectangle on canvas

--- wolfram.js
+++ wolfram.js
@@ -1,4 +1,12 @@
-function displayDate()
+function evaluate()
 {
-  document.getElementById("demo").innerHTML='Carl'
+  var doc = document.getElementById("wolfram");
+  var ctx=doc.getContext("2d");
+  ctx.fillStyle="#FF0000";
+  ctx.fillRect(0,0,150,75);
+}
+
+function makeWolfram()
+{
+  evaluate();
 }
