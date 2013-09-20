1bda341ee6e38f2d845e8200cba06a9933191dd3
Carl Schwope <schwope.carl@gmail.com>
2013-09-20 14:38:28 -0400

    refactor into object / remove interface from js

--- src/wolfram.js
+++ src/wolfram.js
@@ -1,93 +1,72 @@
+function CelluarAutomata( canvas_elem, rules ){
+  this.canvas  = canvas_elem;
+  this.width   = this.canvas.getAttribute('width');
+  this.height  = this.canvas.getAttribute('height');
+  this.context = this.canvas.getContext("2d");
+  this.image   = this.context.createImageData(this.width, this.height);
 
-$(function() {
-  var canvas  = document.getElementById("wolfram");
-  var width   = canvas.getAttribute('width');
-  var height  = canvas.getAttribute('height');
-  var context = canvas.getContext("2d");
-  var image   = context.createImageData(width, height);
+  this.rule_array = rules;
+}
 
-  var rule_array = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
-  ];
+CelluarAutomata.prototype.do_it = function(){
+  // seed the first row
+  this.image.data[this.width * 2 + 3] = 255;
 
-  function pixelAt( row, offset )
+  // run the loops
+  this.celluarAutomata();
+
+  this.context.putImageData(this.image,0,0);
+
+};
+
+CelluarAutomata.prototype.pixelAt = function( row, offset )
+{
+  return ( row * this.width * 4 ) + ( offset  );
+};
+
+CelluarAutomata.prototype.generateRule = function( pixel )
+{
+  var result = 0;
+
+  result += ( this.image.data[ pixel + 7 ] ) ? 1 : 0;
+  result += ( this.image.data[ pixel + 3 ] ) ? 2 : 0;
+  result += ( this.image.data[ pixel - 1 ] ) ? 4 : 0;
+
+  return result;
+};
+
+CelluarAutomata.prototype.genColor = function( rule )
+{
+  return ( rule ? 255 : 0 );
+};
+
+CelluarAutomata.prototype.colorRow = function( row )
+{
+  // the width is the actual pixel width
+  // time four because each pixel is RGBA
+  for ( var i = 0; i < this.width * 4; i += 4 )
   {
-    return ( row * width * 4 ) + ( offset  );
+    // what is the combination from the last row?
+    result = this.generateRule( this.pixelAt( row - 1, i ) );
+
+    // apply the selected color to the current pixel
+    // pixels are zero (RGBA) by default, we only need 0xFF alpha
+      this.image.data[ this.pixelAt( row, i ) + 3 ] = this.genColor( this.rule_array[ result ]);
   }
+};
 
-  function generateRule( pixel )
+CelluarAutomata.prototype.celluarAutomata = function()
+{
+  for (var row = 1; row < this.height; row += 1 )
   {
-    var result = 0;
-
-    result += ( image.data[ pixel + 7 ] ) ? 1 : 0;
-    result += ( image.data[ pixel + 3 ] ) ? 2 : 0;
-    result += ( image.data[ pixel - 1 ] ) ? 4 : 0;
-
-    return result;
-   }
-
-  function genColor( rule )
-  {
-    return ( rule ? 255 : 0 );
+    this.colorRow( row );
   }
+};
 
-  function colorRow( row )
-  {
-    // the width is the actual pixel width
-    // time four because each pixel is RGBA
-    for ( var i = 0; i < width * 4; i += 4 )
-    {
-      // what is the combination from the last row?
-      result = generateRule( pixelAt( row - 1, i ) )
+function makeWolfram(canvas_elem, rule_array_arg)
+{
+  automata = new CelluarAutomata( canvas_elem, rule_array_arg );
+  automata.do_it();
+}
 
-      // apply the selected color to the current pixel
-      // pixels are zero (RGBA) by default, we only need 0xFF alpha
-      image.data[ pixelAt( row, i ) + 3 ] = genColor(rule_array[ result ]);
-    }
-  }
 
-  function celluarAutomata()
-  {
-    for (var row = 1; row < height; row += 1 )
-    {
-      colorRow( row );
-    }
-  }
-
-  function makeWolfram()
-  {
-    // reload the canvas in case of size change
-    width  = canvas.getAttribute('width');
-    height = canvas.getAttribute('height');
-    image  = context.createImageData(width, height);
-
-    // figure out which rules result in black
-    rule_array = [
-      $('.rule_0').is(':checked'),
-      $('.rule_1').is(':checked'),
-      $('.rule_2').is(':checked'),
-      $('.rule_3').is(':checked'),
-      $('.rule_4').is(':checked'),
-      $('.rule_5').is(':checked'),
-      $('.rule_6').is(':checked'),
-      $('.rule_7').is(':checked')
-    ];
-
-    // seed the first row
-    image.data[width * 2 + 3] = 255;
-
-    // run the loops
-    celluarAutomata();
-
-    context.putImageData(image,0,0);
-  }
-
-  $(".commander").click(makeWolfram);
-});
